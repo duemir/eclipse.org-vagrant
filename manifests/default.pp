b362378ca135c00cc5fbe7f7d9ec05819e95764c
Denys Digtiar <duemir@gmail.com>
2013-06-26 14:11:04 +1000

    First working revision of puppet manifest.
    Manifest is updated to setup minimal environment for eclipse sites that
    do not require DB access.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,3 +1,7 @@
+exec {'update':
+    command => '/usr/bin/apt-get update -y',
+}
+
 package {'apache2':
     ensure  => latest,
 }
@@ -9,3 +13,15 @@ package {'php5':
 package {'libapache2-mod-php5':
     ensure  => latest,
 }
+
+service {'apache2':
+    ensure  => running,
+}
+
+file {'/var/www':
+    ensure => link,
+    target => '/vagrant',
+    force  => true,
+}
+
+Exec['update'] -> Package['apache2'] -> Package['php5'] -> Package['libapache2-mod-php5'] -> File['/var/www'] ~> Service['apache2']
